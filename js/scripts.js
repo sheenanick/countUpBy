2b724fb7355ee309fdc782ec22e2418e622bbb9a
Ewa Manek and Sheena Nickerson <ewa@>
2016-08-16 09:11:11 -0700

    Add front-end and back-end logic and error validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
 //<!-- Back End -->
 
+function createOutput(countTo, countBy){
+  var returnArray = [];
+  for(var i = countBy; i <= countTo; i += countBy){
+    console.log(i);
+    returnArray.push(" " + i);
+  }
+  return returnArray;
+}
 
 //<!-- Front End  -->
 $(document).ready(function(){
-
+  $("form").submit(function(event){
+    $("div").removeClass("has-error");
+    event.preventDefault();
+    var countTo = parseInt($("#countTo").val());
+    var countBy = parseInt($("#countBy").val());
+    //console.log(countTo + " " +  countBy);
+    if(!countTo || !countBy || countTo < 0 || countBy < 0 || countBy > countTo){
+      if(!countTo || countTo < 0){
+        $("#countToDiv").addClass("has-error");
+      }
+      if(!countBy || countBy < 0 || countBy > countTo) {
+        $("#countByDiv").addClass("has-error");
+      }
+    }else {
+      var outputArray = createOutput(countTo, countBy);
+      //console.log(outputArray);
+      $(".well").text(outputArray);
+    }
+  });
 });
